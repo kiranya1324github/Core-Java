077582d1c0047f9151d89ff0e7ae2b2cec970a94
kiranya1324github <101395199+kiranya1324github@users.noreply.github.com>
2022-04-26 21:04:56 +0530

    Add files via upload

--- DbConnect.java
+++ DbConnect.java
@@ -1,58 +1,29 @@
-package com.edu;
+package com.edu.school;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-	
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.Statement;
-	import java.util.Scanner;
-
-	public class DbConnect {
-
-		public static void main(String[] args) {
-			//Make a connection with Database
-			int id;
-			String name;
-			
-			Scanner sc=new Scanner(System.in);
-			
-			System.out.println("Enter name");
-			name=sc.next();
-			
-			System.out.println("Enter id");
-			id=sc.nextInt();
-			String driver="com.mysql.cj.jdbc.Driver";
-			String url="jdbc:mysql://localhost:3306/mydb";//mydb is database name
-			String un="root";
-			String pa="root";
-			
-			try {
-				//load the driver
-				Class.forName(driver);
-				Connection conn=DriverManager.getConnection(url,un,pa);
-				Statement st=conn.createStatement();
-			     
-				String ins="insert into login values("+id+",'"+name+"')";
-				int i=st.executeUpdate(ins);
-				if(i>0) {
-					System.out.println("Record inserted");
-				}else {
-					System.out.println("Not inserted");
-				}
-			
-		     }catch(Exception e) {
-				e.printStackTrace();
+		//Database Connection information
+		public class DbConnect {
+		 static String driver="com.mysql.cj.jdbc.Driver";
+		 static String url="jdbc:mysql://localhost:3306/mydb";
+		 static String un="root";
+		 static String pass="root";
+		 static Connection conn=null;
+		 
+		 public static Connection getConnection() {
+			 try {
+				 Class.forName(driver);
+				 conn=DriverManager.getConnection(url, un, pass);
+				 if(conn==null) {
+					 System.out.println("Error in connection");
+				 }
+			 }catch(Exception e) {
+				 e.printStackTrace();
+			 }
+			 return conn;
 			}
-
-		}
-
-	
-
-
-	
-	
-
-	
+		 
 
 	}
 
